0d774bf97dd69b5ee4dae6cec588ea4a99ef3dbc
Franco Cerquetti <fd.cerquetti@gmail.com>
2021-01-09 01:54:56 -0300

    Add pokedex img and score

--- portfolio/src/pokedex/Pokedex.js
+++ portfolio/src/pokedex/Pokedex.js
@@ -1,8 +1,29 @@
 import { React, useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, TextField, CircularProgress } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
+import ScoreIcon from '@material-ui/icons/Score';
+import pokedexImg from './Pokedex.png'
+import './Pokedex.css'
 
 const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokedex/2';
+
+const textFieldStyle = {
+    position: 'absolute',
+    top: '40%',
+    left: '59.5%'
+}
+
+const buttonStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '62.5%'
+}
+
+const iconStyle = {
+    position: 'absolute',
+    top: '58.7%',
+    left: '61.5%'
+}
 
 const Pokedex = () => {
 
@@ -11,6 +32,7 @@ const Pokedex = () => {
     const [userGuess, setUserGuess] = useState('');
     const [correctAnswer, setCorrectAnswer] = useState();
     const [guessButtonDisabled, setGuessButtonDisabled] = useState();
+    const [score, setScore] = useState(0);
     // const [loading, setLoading] = useState(false);
 
     const changePokemon = pokemonArray => {
@@ -20,6 +42,11 @@ const Pokedex = () => {
         setCurrentPokemon(pokemonArray[idx]);
         setUserGuess('');
         // setLoading(true);
+    }
+
+    const nextPokemon = () => {
+        changePokemon(pokemons);
+        setScore(score + 1);
     }
 
     // Fetch pokedex data, only runs at the component mounting
@@ -54,14 +81,24 @@ const Pokedex = () => {
     }, [userGuess, currentPokemon]);
 
     return (
-        <div>
+        <div class='container'>
             { /* <CircularProgress style={{ visibility: loading ? '' : 'hidden' }} /> */ }
+            <img src={pokedexImg} alt='Pokedex' className='pokedex' />
             { currentPokemon ?
-                <img src={currentPokemon.sprite} alt={ currentPokemon.name } /*onLoad={() => setLoading(false)}*/ />
+                <img src={currentPokemon.sprite} alt={ currentPokemon.name } className='pokemon' /*onLoad={() => setLoading(false)}*/ />
                 : null
             }
-            <TextField id="standard-basic" value={userGuess} error={!correctAnswer} onChange={ e => setUserGuess(e.target.value) } />
-            <Button variant="contained" disabled={guessButtonDisabled} onClick={ () => changePokemon(pokemons) } >Next!</Button>
+            <TextField style={textFieldStyle} value={userGuess} error={!correctAnswer} onChange={ e => setUserGuess(e.target.value) } />
+            <Button 
+                style={buttonStyle}
+                variant='contained'
+                disabled={guessButtonDisabled}
+                color='primary'
+                onClick={ () => nextPokemon() } >
+                    Next!
+            </Button>
+            <ScoreIcon style={iconStyle} />
+            <label className='score'>Score: {score}</label>
         </div>
     );
 }
